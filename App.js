c7a42510e876f063985dc71cb52e87809617102b
mostlyali07 <104094853+mostlyali07@users.noreply.github.com>
2023-01-13 17:45:16 +0500

    Update

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+import FirstHead from "./Components/FirstHead";
+
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <FirstHead />
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ Components/FirstHead.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FirstHead = () => {
+  return (
+    <View>
+      <Text>FirstHead</Text>
+    </View>
+  )
+}
+
+export default FirstHead;
