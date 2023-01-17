93a77f1f7c25a800de9cc6b046e5828ea2ba8468
mostlyali07 <104094853+mostlyali07@users.noreply.github.com>
2023-01-17 17:07:19 +0500

    Update

--- Components/FirstHead.js
+++ Components/FirstHead.js
@@ -1,13 +1,13 @@
 import { Text, View} from 'react-native';
 import React from 'react';
-import SideBar from "SideBar";
+// import SideBar from "SideBar";
 
 const FirstHead = () => {
   return (
-    <View style={{backgroundColor: '#000', padding: 15}}>
+    <View style={{backgroundColor: '#000', padding: 12}}>
       <Text style={{color: 'white'}}>Deliver to:</Text>
       <Text style={{color: 'white', fontWeight: 500}}>HOME</Text>
-      <SideBar />
+      {/* <SideBar /> */}
     </View>
   );
 };
