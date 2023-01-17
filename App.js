ce3c0d03d548517819aee0d7720b3a68698857cd
mostlyali07 <104094853+mostlyali07@users.noreply.github.com>
2023-01-17 20:45:49 +0500

    Update

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
-import FirstHead from "./Components/FirstHead";
+// import React from 'react';
+// import {SafeAreaView, View, Text} from 'react-native';
+// import FirstHead from "./Components/FirstHead";
 
+// const App = () => {
+//   return (
+//     <SafeAreaView>
+//       <FirstHead />
+//     </SafeAreaView>
+//   );
+// };
 
-const App = () => {
+// export default App;
+
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import Home from './Screens/Home';
+import Profile from './Screens/Profile';
+import {NavigationContainer} from '@react-navigation/native';
+
+const Drawer = createDrawerNavigator();
+
+function App() {
   return (
-    <SafeAreaView>
-      <FirstHead />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Feed" component={Home} />
+        <Drawer.Screen name="Article" component={Profile} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
-};
+}
 
 export default App;
